d944877e51133f848d92ce733ab51a27d714094f
victoriacutino <victoria.cutino@gmail.com>
2021-09-27 21:28:48 -0300

    repo-mercado-liebre

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const app = express ();
+const path = require ('path');
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.get('/', (req,res)=>{
+    res.sendFile (path.resolve(__dirname,'./views/home.html'));
+})
+
+app.get('/register', (req,res)=>{
+    res.sendFile (path.resolve(__dirname,'./views/register.html'));
+})
+
+app.get('/login', (req,res)=>{
+    res.sendFile (path.resolve(__dirname,'./views/login.html'));
+})
+
+app.post('/register', (req,res)=>{
+    res.sendFile (path.resolve(__dirname,'./views/home.html'));
+})
+
+app.post('/login', (req,res)=>{
+    res.sendFile (path.resolve(__dirname,'./views/home.html'));
+})
+
+app.listen (3000 || process.env.PORT, ()=>{
+    console.log ('Server corriendo en el puerto 3000');
+});
+
+
